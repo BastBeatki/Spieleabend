6fa41013e441c10352d8e186adf0870f0b9c1eea
BastBeatki <136461423+BastBeatki@users.noreply.github.com>
2026-03-15 12:46:38 +0100

    Create firebaseConfig.ts

---
+++ services/firebaseConfig.ts
@@ -0,0 +1,19 @@
+
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAApVR7I-cj6Fvsy689qGQW1bF1cUAnanc",
+  authDomain: "spieleabend-scoreboard.firebaseapp.com",
+  projectId: "spieleabend-scoreboard",
+  storageBucket: "spieleabend-scoreboard.firebasestorage.app",
+  messagingSenderId: "712352309922",
+  appId: "1:712352309922:web:754407457ce5e0dd1f2dfb"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth, firebaseConfig };
